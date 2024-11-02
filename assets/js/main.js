b93ce68fb0b9b45c714878235aea75dc17512db6
mdmunnaahmed <msdmunna77@gmail.com>
2024-11-02 16:57:18 +0600

    gradient update

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  // Wow Effects
+  wow = new WOW({
+    boxClass: "wow", // default
+    animateClass: "animated", // default
+    offset: 100, // default
+    mobile: true, // default
+    live: true, // default
+  });
+  wow.init();
+});
