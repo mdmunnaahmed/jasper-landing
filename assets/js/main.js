5eb48df1898e9938d7ed0821d261cca78441c310
mdmunnaahmed <msdmunna77@gmail.com>
2024-11-02 13:48:13 +0600

    3d section --done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,27 @@ $(document).ready(function () {
   wow = new WOW({
     boxClass: "wow", // default
     animateClass: "animated", // default
-    offset: 100, // default
+    offset: 80, // default
     mobile: true, // default
     live: true, // default
   });
   wow.init();
+});
+
+const wrapper = document.querySelector(".feature-grid");
+const cards = document.querySelectorAll(".feature-item");
+
+wrapper.addEventListener("mousemove", function ($event) {
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect();
+    const x = $event.clientX - rect.left;
+    const y = $event.clientY - rect.top;
+
+    card.style.setProperty("--xPos", `${x}px`);
+    card.style.setProperty("--yPos", `${y}px`);
+  });
+});
+
+document.querySelector("#joinBtn").addEventListener("click", function (e) {
+  document.querySelector("#emailInput").focus();
 });
