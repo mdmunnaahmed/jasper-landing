c3d6b55722f849395c579738d24296b9d55c851a
mdmunnaahmed <msdmunna77@gmail.com>
2024-11-11 21:06:57 +0600

    remove stop scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,28 +28,28 @@ document.querySelector("#joinBtn").addEventListener("click", function (e) {
   document.querySelector("#emailInput").focus();
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const scrollSection = document.getElementById("scroll-section");
-  const scrollingText = document.querySelector(".scrolling-text");
-  let isScrolled = false; // Prevent re-triggering
+// document.addEventListener("DOMContentLoaded", function () {
+//   const scrollSection = document.getElementById("scroll-section");
+//   const scrollingText = document.querySelector(".scrolling-text");
+//   let isScrolled = false; // Prevent re-triggering
 
-  // Scroll event to trigger fade-in effect
-  window.addEventListener("scroll", function () {
-    const sectionTop = scrollSection.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+//   // Scroll event to trigger fade-in effect
+//   window.addEventListener("scroll", function () {
+//     const sectionTop = scrollSection.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight;
 
-    // Trigger when scroll-section comes into view
-    if (sectionTop <= windowHeight * 0.95 && !isScrolled) {
-      isScrolled = true;
-      document.documentElement.style.overflowY = "hidden"; // Disable scrolling temporarily
-      scrollingText.style.opacity = 1; // Fade in the text
+//     // Trigger when scroll-section comes into view
+//     if (sectionTop <= windowHeight * 0.95 && !isScrolled) {
+//       isScrolled = true;
+//       // document.documentElement.style.overflowY = "hidden"; // Disable scrolling temporarily
+//       scrollingText.style.opacity = 1; // Fade in the text
 
-      // Re-enable scrolling after the fade-in completes
-      setTimeout(() => {
-        document.documentElement.style.overflowY = "auto";
-      }, 5000); // Matches the fade-in duration
-    }
-  });
-});
+//       // Re-enable scrolling after the fade-in completes
+//       setTimeout(() => {
+//         // document.documentElement.style.overflowY = "auto";
+//       }, 5000); // Matches the fade-in duration
+//     }
+//   });
+// });
 
 
